c5ab2cd4bbeaabc70b7c6760de5b218203526612
Islam Günebakan <79874443+Gunebakan27@users.noreply.github.com>
2022-02-05 16:52:00 +0100

    first

---
+++ src/main/java/thymeleaf/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package thymeleaf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import thymeleaf.model.Employee;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
